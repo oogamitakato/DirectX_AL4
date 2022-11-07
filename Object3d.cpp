98db37ec15e8361f19430f26d8d77fa21aacd0bb
TakatoOogami <k021g1156@g.neec.ac.jp>
2022-11-07 16:07:34 +0900

    02_03まで実装

--- Object3d.cpp
+++ Object3d.cpp
@@ -29,10 +29,10 @@ XMFLOAT3 Object3d::eye = { 0, 0, -50.0f };
 XMFLOAT3 Object3d::target = { 0, 0, 0 };
 XMFLOAT3 Object3d::up = { 0, 1, 0 };
 D3D12_VERTEX_BUFFER_VIEW Object3d::vbView{};
-D3D12_INDEX_BUFFER_VIEW Object3d::ibView{};
+//D3D12_INDEX_BUFFER_VIEW Object3d::ibView{};
 Object3d::VertexPosNormalUv Object3d::vertices[vertexCount];
 //unsigned short Object3d::indices[planeCount * 3];
-unsigned short Object3d::indices[indexCount];
+//unsigned short Object3d::indices[indexCount];
 XMMATRIX Object3d::matBillbord = XMMatrixIdentity();
 XMMATRIX Object3d::matBillbordY = XMMatrixIdentity();
 
@@ -73,7 +73,7 @@ void Object3d::PreDraw(ID3D12GraphicsCommandList * cmdList)
 	// ルートシグネチャの設定
 	cmdList->SetGraphicsRootSignature(rootsignature.Get());
 	// プリミティブ形状を設定
-	cmdList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	cmdList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_POINTLIST);
 }
 
 void Object3d::PostDraw()
@@ -320,7 +320,7 @@ void Object3d::InitializeGraphicsPipeline()
 	gpipeline.InputLayout.NumElements = _countof(inputLayout);
 
 	// 図形の形状設定（三角形）
-	gpipeline.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	gpipeline.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_POINT;
 
 	gpipeline.NumRenderTargets = 1;	// 描画対象は1つ
 	gpipeline.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB; // 0～255指定のRGBA
@@ -365,7 +365,7 @@ void Object3d::LoadTexture()
 	ScratchImage scratchImg{};
 
 	// WICテクスチャのロード
-	result = LoadFromWICFile( L"Resources/texture.png", WIC_FLAGS_NONE, &metadata, scratchImg);
+	result = LoadFromWICFile( L"Resources/tex1.png", WIC_FLAGS_NONE, &metadata, scratchImg);
 	assert(SUCCEEDED(result));
 
 	ScratchImage mipChain{};
@@ -433,16 +433,21 @@ void Object3d::CreateModel()
 {
 	HRESULT result = S_FALSE;
 
-	//四角形の頂点データ
-	VertexPosNormalUv verticesSquare[] = {
-		{{-5.0f,-5.0f,0.0f},{0,0,1},{0,1}},//左下
-		{{-5.0f,+5.0f,0.0f},{0,0,1},{0,0}},//左上
-		{{+5.0f,-5.0f,0.0f},{0,0,1},{1,1}},//右上
-		{{+5.0f,+5.0f,0.0f},{0,0,1},{1,0}},//右下
-	};
+	////四角形の頂点データ
+	//VertexPosNormalUv verticesSquare[] = {
+	//	{{-5.0f,-5.0f,0.0f},{0,0,1},{0,1}},//左下
+	//	{{-5.0f,+5.0f,0.0f},{0,0,1},{0,0}},//左上
+	//	{{+5.0f,-5.0f,0.0f},{0,0,1},{1,1}},//右上
+	//	{{+5.0f,+5.0f,0.0f},{0,0,1},{1,0}},//右下
+	//};
 
 	//メンバ変数にコピー
-	std::copy(std::begin(verticesSquare), std::end(verticesSquare), vertices);
+	//std::copy(std::begin(verticesSquare), std::end(verticesSquare), vertices);
+
+	VertexPosNormalUv verticesPoint[] = {
+		{{0.0f,0.0f,0.0f},{0,0,1},{0,1}},
+	};
+	std::copy(std::begin(verticesPoint), std::end(verticesPoint), vertices);
 
 	//四角形のインデックスデータ
 	unsigned short indicesSquare[] = {
@@ -451,7 +456,7 @@ void Object3d::CreateModel()
 	};
 
 	//メンバ変数にコピー
-	std::copy(std::begin(indicesSquare), std::end(indicesSquare), indices);
+	//std::copy(std::begin(indicesSquare), std::end(indicesSquare), indices);
 
 	UINT sizeVB = static_cast<UINT>(sizeof(vertices));
 
@@ -479,9 +484,9 @@ void Object3d::CreateModel()
 	vbView.SizeInBytes = sizeof(vertices);
 	vbView.StrideInBytes = sizeof(vertices[0]);
 
-	UINT sizeIB = static_cast<UINT>(sizeof(indices));
+	//UINT sizeIB = static_cast<UINT>(sizeof(indices));
 	// リソース設定
-	resourceDesc.Width = sizeIB;
+	//resourceDesc.Width = sizeIB;
 
 	// インデックスバッファ生成
 	result = device->CreateCommittedResource(
@@ -494,18 +499,18 @@ void Object3d::CreateModel()
 	if (SUCCEEDED(result)) {
 
 		// 全インデックスに対して
-		for (int i = 0; i < _countof(indices); i++)
-		{
-			indexMap[i] = indices[i];	// インデックスをコピー
-		}
+		//for (int i = 0; i < _countof(indices); i++)
+		//{
+		//	indexMap[i] = indices[i];	// インデックスをコピー
+		//}
 
 		indexBuff->Unmap(0, nullptr);
 	}
 
 	// インデックスバッファビューの作成
-	ibView.BufferLocation = indexBuff->GetGPUVirtualAddress();
-	ibView.Format = DXGI_FORMAT_R16_UINT;
-	ibView.SizeInBytes = sizeof(indices);
+	//ibView.BufferLocation = indexBuff->GetGPUVirtualAddress();
+	//ibView.Format = DXGI_FORMAT_R16_UINT;
+	//ibView.SizeInBytes = sizeof(indices);
 }
 
 void Object3d::UpdateViewMatrix()
@@ -663,7 +668,7 @@ void Object3d::Draw()
 	// 頂点バッファの設定
 	cmdList->IASetVertexBuffers(0, 1, &vbView);
 	// インデックスバッファの設定
-	cmdList->IASetIndexBuffer(&ibView);
+	//cmdList->IASetIndexBuffer(&ibView);
 
 	// デスクリプタヒープの配列
 	ID3D12DescriptorHeap* ppHeaps[] = { descHeap.Get() };
@@ -674,5 +679,6 @@ void Object3d::Draw()
 	// シェーダリソースビューをセット
 	cmdList->SetGraphicsRootDescriptorTable(1, gpuDescHandleSRV);
 	// 描画コマンド
-	cmdList->DrawIndexedInstanced(3, 1, 0, 0, 0);
+	//cmdList->DrawIndexedInstanced(3, 1, 0, 0, 0);
+	cmdList->DrawInstanced(_countof(vertices), 1, 0, 0);
 }
--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,30 +1,44 @@
 #include "BasicShaderHeader.hlsli"
 
-[maxvertexcount(6)]
+[maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION, 
+	point VSOutput input[1] : SV_POSITION, 
 	inout TriangleStream< GSOutput > output
 )
 {
-	//一つ目の三角形
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv * 2.0f;
-		output.Append(element);
-	}
-	//現在のストリップを終了
-	output.RestartStrip();
+	GSOutput element;
+	//共通
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+	//1点目
+	element.svpos = input[0].svpos;
+	output.Append(element);
+	//2点目
+	element.svpos = input[0].svpos + float4(10.0f,10.0f,0,0);
+	output.Append(element);
+	//3点目
+	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
+	output.Append(element);
 
-	//二つ目の三角形
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos + float4(20.0f,0.0f,0.0f,0.0f);
-		element.normal = input[i].normal;
-		element.uv = input[i].uv * 5.0f;
-		output.Append(element);
-	}
+	////一つ目の三角形
+	//for (uint i = 0; i < 3; i++)
+	//{
+	//	GSOutput element;
+	//	element.svpos = input[i].svpos;
+	//	element.normal = input[i].normal;
+	//	element.uv = input[i].uv * 2.0f;
+	//	output.Append(element);
+	//}
+	////現在のストリップを終了
+	//output.RestartStrip();
+
+	////二つ目の三角形
+	//for (uint i = 0; i < 3; i++)
+	//{
+	//	GSOutput element;
+	//	element.svpos = input[i].svpos + float4(20.0f,0.0f,0.0f,0.0f);
+	//	element.normal = input[i].normal;
+	//	element.uv = input[i].uv * 5.0f;
+	//	output.Append(element);
+	//}
 }
